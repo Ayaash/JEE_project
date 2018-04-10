3e98a24221b9f7d35c65e6f1f2ef2e408cb2601e
Unknown <gtibal@gmail.com>
2018-04-11 00:18:39 +0200

    Auto stash before merge of "master" and "origin/master"
    insertion de partie faite

--- JEE-project/src/pack/LienMySQL.java
+++ JEE-project/src/pack/LienMySQL.java
@@ -379,6 +379,8 @@ public class LienMySQL {
 		}
 	}
 	
+	
+	
 	//fonction des parties
 	public List<Partie> findParties() {
 		getConnection();
@@ -419,6 +421,23 @@ public class LienMySQL {
 		}
 		return listpart;
 
+	}
+	
+	public int insererPartie(Partie p) {
+		getConnection();
+		int id = -1;
+		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String debut = sdf.format(p.getDebut());
+		String fin = sdf.format(p.getFin());
+		
+		String requete = "INSERT INTO partie VALUE (DEFAULT, \"" + p.getJoueur().getId() + "\", \"" + p.getJeu().getId() + "\",\"" + debut + "\", \"" + fin + "\");";
+		this.executerUpdate(requete);
+		
+
+		fermerConnections();
+		
+		
+		return id;		
 	}
 
 }
